b31693656c9908e5ee4eb33447787e5a2f7cda8c
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-08-26 11:56:35 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'secrets/new'
 
-  # root 'application#hello'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
